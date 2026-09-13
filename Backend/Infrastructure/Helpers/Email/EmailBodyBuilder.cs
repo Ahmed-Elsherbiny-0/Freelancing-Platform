3f0301fdf41ed9d2b85415b4c82c075fae95c6ef
AhmedElsherbiny2 <140967751+AhmedElsherbiny2@users.noreply.github.com>
2026-09-13 15:47:26 +0300

    fix directory  error

--- Backend/Infrastructure/Helpers/Email/EmailBodyBuilder.cs
+++ Backend/Infrastructure/Helpers/Email/EmailBodyBuilder.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,20 +11,22 @@ namespace Infrastructure.Helpers.Email
     public  class EmailBodyBuilder(IWebHostEnvironment env):IEmailBodyBuilder
     {
         private  readonly IWebHostEnvironment _env = env;
-
-        public  string GenerateEmailBody(string templete, Dictionary<string, string> dictioary)
+        public string GenerateEmailBody(string templete, Dictionary<string, string> dictioary)
         {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = $"Infrastructure.Services.Email.Templates.{templete}.html";
 
-            var templetePath = $"{Path.Combine(_env.ContentRootPath,"../" ,"Infrastructure")}/Services/Email/Templates/{templete}.html";
-           using var stream=new StreamReader(templetePath);
-            var emailbody=stream.ReadToEnd();
+            using var stream = assembly.GetManifestResourceStream(resourceName)
+                ?? throw new FileNotFoundException($"Email template not found: {resourceName}");
+            using var reader = new StreamReader(stream);
+            var emailBody = reader.ReadToEnd();
 
             foreach (var item in dictioary)
             {
-            emailbody=emailbody.Replace(item.Key, item.Value);
+                emailBody = emailBody.Replace(item.Key, item.Value);
             }
 
-            return emailbody;
-        } 
+            return emailBody;
+        }
     }
 }
